05a9cfad0610d35344a7bad877d4802346fe93ea
Eddie Martinez <elmartinezinfo@users.noreply.github.com>
2014-03-12 22:29:11 -0700

    index
    init

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="ie6 oldie"> <![endif]-->
+<!--[if IE 7]>    <html class="ie7 oldie"> <![endif]-->
+<!--[if IE 8]>    <html class="ie8 oldie"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Edward L Martinez | UI Design and Development | Cureently Using: </title>
+<link href="css/boilerplate.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link rel="icon" type="image/ico" href="fav.ico" />
+<link rel="icon" type="image/png" href="fav.png" />
+<script src="js/custom.modernizr.js"></script>
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<script src="js/respond.min.js"></script>
+</head>
+
+<body>
+	<div class="LeftPanel">
+        <div>
+            <span class="headermain">
+                Eddie L Martinez
+            </span>
+        </div>
+        <div>
+            <span class="headermainsub1">
+                Titles and Positions<br /> 
+                Held: 2003 - Present
+            </span>
+        </div>
+        <div>
+            <span class="headermainsub2">
+                UI Design and Development
+            </span>
+        </div>
+    </div>
+</body>
+</html>
